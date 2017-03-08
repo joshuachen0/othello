8f4626e83e3dfb50d56a1ef976945c3ca3fa6be7
joshuachen0 <joshua.chen@caltech.edu>
2017-03-07 19:43:36 -0800

    Beats BetterPlayer.

--- player.cpp
+++ player.cpp
@@ -9,34 +9,15 @@ Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
 
-    /*
-     * TODO: Do any initialization you need to do here (setting up the board,
-     * precalculating things, etc.) However, remember that you will only have
-     * 30 seconds.
-     */
     board = new Board();
-    mySide = side;
-    if(mySide == BLACK){opSide = WHITE;}
-    else{opSide = BLACK;}
-    /**
-    for(int i = 0; i < 8; i++){
-        for(int j = 0; j < 8; j++){
-            if(i == 0 || i == 7 || j == 0 || j == 7){
-                weightTable[i][j] = 3;
-            }
-            else if(i == 1 || i == 6 || j == 1 || j == 6){
-                weightTable[i][j] = -3;
-            }
-            else if(i == 2 || i == 5 || j == 2 || j == 5){
-                weightTable[i][j] = 1;
-            }
-            else{
-                weightTable[i][j] = -1;
-            }
-        }
-    }
-    **/
 
+    mySide = side;
+    if (mySide == BLACK) {
+        opSide = WHITE;
+    }
+    else {
+        opSide = BLACK;
+    }
 }
 
 /*
@@ -96,50 +77,38 @@ Move *Player::bestMove(vector <Move*> m, Board * b){
     int themove = 0;
     int max = -9999999;
     Board * copy;
-    for(int i = 0; i < m.size(); i++){
+    for(int i = 0; i < m.size(); i++) {
         copy = b->copy();
         copy->doMove(m[i],mySide);
         vector <Move *> opLegalMoves = legalMoves(copy, opSide);
         int min = 99999; 
         Board * copy2;
-        for(int j = 0; j < opLegalMoves.size(); j++){
+        for(int j = 0; j < opLegalMoves.size(); j++) {
             copy2 = copy->copy();
             copy2->doMove(opLegalMoves[j],opSide);
             vector <Move *> myLegalMoves2 = legalMoves(copy2,mySide);
             Board * copy3;
-            for(int k = 0; k < myLegalMoves2.size(); k++){
+            for(int k = 0; k < myLegalMoves2.size(); k++) {
                 copy3 = copy2->copy();
                 copy3->doMove(myLegalMoves2[k],mySide);
                 vector <Move *> opLegalMoves2 = legalMoves(copy3, opSide);
                 Board * copy4;
-                for(int w = 0; w < opLegalMoves2.size(); w++){
+                for(int w = 0; w < opLegalMoves2.size(); w++) {
                     copy4 = copy3->copy();
                     copy4->doMove(opLegalMoves2[w],opSide);
                     if(getScore(copy4) < min){
                         min = getScore(copy4);
                     }
-
                 }
-
             }
-            // if(getScore(copy2) < min){
-            //     min = getScore(copy2);
-            // }
-            /*
-            cerr << "Blacks: " << copy2->countBlack() << endl;
-            cerr << "Whites: " << copy2->countWhite() << endl;
-            cerr << "This one got" << getScore(copy2) << endl;
-            */
             delete copy2;
         }
-        //cerr << min << endl;
         if(min > max){
             max = min;
             themove = i; 
         }
         delete copy;
     }
-    //cerr << "best: " << max << endl;
     return m[themove];
 }
 
